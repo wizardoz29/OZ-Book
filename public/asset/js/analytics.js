106b6e9161f37bed1bea133b7bc5f4b09ced828d
Zunurain <zunurainojuolape@gmail.com>
2022-10-30 00:34:38 +0100

    Corrected my error, added changes to JS and HTML

--- public/asset/js/analytics.js
+++ public/asset/js/analytics.js
@@ -1,3 +1,5 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
